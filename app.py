1bb2fc9f5f10f1a7a4d518e6648ea4ff76a352f5
forever2k <forever2k@yandex.ru>
2021-03-31 21:14:07 +0300

    1

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
+import os
 import telebot
 from flask import Flask, request
-import os
 import random
-import requests
+
 
 TOKEN = '1668076384:AAF4aDoDF6_KqaADNtgGjUIdgykdVAM4qtI'
 APP_NAME = 'https://error-bot-check.herokuapp.com/'
@@ -69,21 +69,22 @@ def repeat_all_messages(message):
 
 
 
-
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
     return "it works", 200
+
 
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url = APP_NAME + TOKEN)
+    bot.set_webhook(url=APP_NAME + TOKEN)
     return "it worksssssssss", 200
 
 
-if __name__ == '__main__':
-    server.debug = True
+if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
 
